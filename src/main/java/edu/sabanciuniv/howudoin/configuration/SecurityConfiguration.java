f7f1f87deb04d6118e6041eec9795ca0328f80d4
Fırat Batar <firat.o.batar@gmail.com>
2024-11-12 12:54:02 +0300

    Add security configs

---
+++ src/main/java/edu/sabanciuniv/howudoin/configuration/SecurityConfiguration.java
@@ -0,0 +1,41 @@
+package edu.sabanciuniv.howudoin.configuration;
+
+import edu.sabanciuniv.howudoin.component.JwtAuthEntryPoint;
+import edu.sabanciuniv.howudoin.component.JwtAuthFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfiguration {
+    private final JwtAuthEntryPoint authEntryPoint;
+    private final JwtAuthFilter jwtAuthFilter;
+
+    @Autowired
+    public SecurityConfiguration(
+            JwtAuthEntryPoint authEntryPoint,
+            JwtAuthFilter jwtAuthFilter
+    ) {
+        this.authEntryPoint = authEntryPoint;
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .authorizeRequests()
+                .requestMatchers("/register", "/login").permitAll()
+                .anyRequest().authenticated()
+                .and().exceptionHandling(ex -> ex.authenticationEntryPoint(authEntryPoint))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        httpSecurity.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
+    }
+}
