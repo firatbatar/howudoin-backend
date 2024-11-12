2e0739f9c66d0f24debff384d01c9632a4f3003a
Fırat Batar <firat.o.batar@gmail.com>
2024-11-12 11:00:26 +0300

    Add LoginModel and LoginResponse

---
+++ src/main/java/edu/sabanciuniv/howudoin/model/LoginResponse.java
@@ -0,0 +1,15 @@
+package edu.sabanciuniv.howudoin.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponse {
+    private String token;
+    private String email;
+}
