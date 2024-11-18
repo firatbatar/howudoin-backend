d043b36442fd2750ff4c26fc1d592486b31a6036
Fırat Batar <firat.o.batar@gmail.com>
2024-11-18 22:58:55 +0300

    Format

--- src/main/java/edu/sabanciuniv/howudoin/controller/MessageController.java
+++ src/main/java/edu/sabanciuniv/howudoin/controller/MessageController.java
@@ -5,7 +5,11 @@ import edu.sabanciuniv.howudoin.model.MessageModel;
 import edu.sabanciuniv.howudoin.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
